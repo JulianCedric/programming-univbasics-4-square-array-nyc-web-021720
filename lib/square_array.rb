725918849331a3bc613ab650663341534678e4aa
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-10 22:40:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   numbers = [1,2,3]
   counter = 0 
   while counter < 3
-  return (numbers[counter])**(numbers[counter])
+  puts (numbers[counter])**(numbers[counter])
   counter += 1 
   end
 end 
